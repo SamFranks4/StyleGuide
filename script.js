3a2e2b2413c5b9e6fd0402b55a5ee85335ff2584
SamFranks4 <samfranks2007@gmail.com>
2025-11-03 16:24:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Only highlight the current page link
+// Highlight the active sidebar link based on current page
 const sidebarLinks = document.querySelectorAll('.sidebar-links a');
 
-// Highlight based on current page URL
+// Get the current page filename (e.g., "index.html" or "researchpage.html")
 const currentPage = window.location.pathname.split("/").pop();
 
 sidebarLinks.forEach(link => {
